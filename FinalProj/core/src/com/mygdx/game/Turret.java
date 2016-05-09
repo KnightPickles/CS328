2291b56a7fa87cce84d056b7122570ad0c17ed77
yama4272 <KnightPickles@Knights-MacBook-Pro.local>
2016-05-09 03:16:06 -0700

    Fixed i think

--- FinalProj/core/src/com/mygdx/game/Turret.java
+++ FinalProj/core/src/com/mygdx/game/Turret.java
@@ -21,7 +21,8 @@ public class Turret extends GameObject {
 	GameObject target; //What we're targetting
 	float attackCooldown = 0;
 	public int goldValue = 0; //Gold spent on this turret
-	
+	public double costAccumulator = 1.1;
+	public int upgradeCost = 10;
 	Sound[] redSounds = new Sound[3];
 	Sound[] greenSounds = new Sound[3];
 	Sound[] blueSounds = new Sound[3];
@@ -34,7 +35,20 @@ public class Turret extends GameObject {
 		Green,
 		Blue
 	}
-	
+
+	public Turret(Turret t) {
+		super((GameObject)t);
+		myInfo = new TurretInfo(t.myInfo);
+		rotateSprite = new Sprite(t.sprite);
+		targetFinder = new Circle(t.targetFinder);
+		target = new GameObject(target);
+		attackCooldown = t.attackCooldown;
+		goldValue = t.goldValue;
+		costAccumulator = t.costAccumulator;
+		upgradeCost = t.upgradeCost;
+		turretType = t.turretType;
+	}
+
 	public Turret(TurretInfo info, Vector2 spawnPos) {
 		myInfo = info;
 		sprite = MainGameClass._instance.atlas.createSprite(myInfo.spriteName);
