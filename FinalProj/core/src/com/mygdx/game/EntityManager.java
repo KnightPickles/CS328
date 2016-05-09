e1a36f47130cc464ba75d00f5a3e979adfcf876d
yama4272 <KnightPickles@Knights-MacBook-Pro.local>
2016-05-08 22:48:00 -0700

    Derp

--- FinalProj/core/src/com/mygdx/game/EntityManager.java
+++ FinalProj/core/src/com/mygdx/game/EntityManager.java
@@ -44,16 +44,16 @@ public class EntityManager {
     }
 
     public boolean buildTurret(String turretName, Vector2 position) {
-        if(Player.gold >= turretTable.get(turretName).cost) {
+        //if(Player.gold >= turretTable.get(turretName).cost) {
             System.out.println("Spent " + turretTable.get(turretName).cost + " gold on a " + turretName);
-            Player.gold -= turretTable.get(turretName).cost;
+            //Player.gold -= turretTable.get(turretName).cost;
             Turret turret = new Turret(turretTable.get(turretName), position);
             turrets.add(turret);
             return true;
-        } else {
-            System.out.println(Player.gold + " is not enough to buy a ballista. It costs " + turretTable.get("red0").cost + ".");
-            return false;
-        }
+        //} else {
+        //    System.out.println(Player.gold + " is not enough to buy a ballista. It costs " + turretTable.get("red0").cost + ".");
+        //    return false;
+        //}
     }
     
     public void spawnTurret(String turretName, Vector2 position) {
